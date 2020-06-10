0014daf44b9d52b22ab74621c13a7c6f19ceb9ad
Erik <erik.camenzuli@hotmail.com>
2020-06-11 09:33:14 +1000

    day before beta

--- RunJumpDodge/Assets/Scripts/ScoreScript.cs
+++ RunJumpDodge/Assets/Scripts/ScoreScript.cs
@@ -7,12 +7,20 @@ public class ScoreScript : MonoBehaviour
     public Text text;
     public Text multiplier;
     public Text tokenText;
+    public Text highScore;
 
     private int tokens;
     private float score = 0.0f;                 // - scoring
     private int difficulty = 1;                 // - how hard a level is
     private int maxDifficulty = 100;            // - max difficulty
     private int nextLevel = 1000;                 // - how much score is needed for next level 
+
+
+    void Start()
+    {
+       // highScore.text = PlayerPrefs.GetInt("Highscore", 0).ToString();
+    }
+
 
     // Update is called once per frame
     void Update()
@@ -26,6 +34,17 @@ public class ScoreScript : MonoBehaviour
         
         //increasing score over time then multipling by whatever the difficulty level is
         score += Time.deltaTime * difficulty;
+
+        //text.text = score.ToString();
+       // if(score > PlayerPrefs.GetInt("Highscore", 0))
+       // {
+       //     PlayerPrefs.SetInt("Highscore", (int)score);
+       //     highScore.text = score.ToString();
+       // }
+       // else
+       // {
+       //     return;
+       // }
 
         //setting the text component to the score
         //initalizng score as a int value and convert the values after the . value of the float
